77b2c512db01546d38b097f6b8b9e5f2b6de7704
pholubtsov <110049219+pholubtsov@users.noreply.github.com>
2024-10-29 17:23:20 +0200

    .

--- modules/aws-asg/change-index-html.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-echo \"$(hostname -f)\" > /var/www/html/index.html
--- modules/providers/main.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0" # Use any compatible version in 5.x series
-    }
-  }
-}
